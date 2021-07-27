47d02d50d6a4dfcad64ef5e137253cfbdd04ca41
Aditya <aditya.net09@gmail.com>
2021-07-27 10:06:48 -0700

    added image description

--- calvinhobbes.py
+++ calvinhobbes.py
@@ -12,5 +12,14 @@ soup = BeautifulSoup(page.content, 'lxml')
 #print(soup.prettify())
 
 #extracting image url class_=img-fluid lazyloaded
+#image-url
+# <a itemprop="image" class="js-item-comic-link item-comic-link-disabled" title="Calvin and Hobbes">
+
+#   <picture class="item-comic-image"></picture>
+# <img class="img-fluid lazyloaded" srcset="https://assets.amuniversal.com/59ee0c705e0a012ee3bf00163e41dd5b 600w" data-srcset="https://assets.amuniversal.com/59ee0c705e0a012ee3bf00163e41dd5b 600w" sizes="
+#                        (min-width: 992px) 600px,
+#                        (min-width: 768px) 600px,
+#                        (min-width: 576px) 300px,
+#                        600px" alt="Calvin and Hobbes Comic Strip for August 06, 2010 " src="https://assets.amuniversal.com/59ee0c705e0a012ee3bf00163e41dd5b" width="100%"></a>
 imgUrl =  soup.find('img', 'img-fluid lazyloaded')
 print(imgUrl)
