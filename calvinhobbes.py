8191236344866bae836b5d04990edb5d5586b06b
Aditya <aditya.net09@gmail.com>
2021-07-27 09:57:02 -0700

    img url

--- calvinhobbes.py
+++ calvinhobbes.py
@@ -6,3 +6,11 @@ from bs4 import BeautifulSoup
 
 page = requests.get('https://www.gocomics.com/random/calvinandhobbes')
 print(page.url)
+
+#BeautifulSoup object
+soup = BeautifulSoup(page.content, 'lxml')
+#print(soup.prettify())
+
+#extracting image url class_=img-fluid lazyloaded
+imgUrl =  soup.find('img', 'img-fluid lazyloaded')
+print(imgUrl)
