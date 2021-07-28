2c2196da970494d83548e94001f6910cf4d3a4dd
Aditya <aditya.net09@gmail.com>
2021-07-27 21:59:11 -0700

    image url working

--- calvinhobbes.py
+++ calvinhobbes.py
@@ -18,8 +18,8 @@ soup = BeautifulSoup(page.content, 'lxml')
 #                        (min-width: 768px) 600px,
 #                        (min-width: 576px) 300px,
 #                        900px" src="https://assets.amuniversal.com/d5712420c125012fdd6d001dd8b71c47" srcset="https://assets.gocomics.com/assets/transparent-3eb10792d1f0c7e07e7248273540f1952d9a5a2996f4b5df70ab026cd9f05517.png" width="100%"/></picture>
-imgUrl =  soup.find('picture', 'item-comic-image')
-print(imgUrl)
-img = imgUrl.find("img")
-imgThing = img['src']
-print(imgThing)
+img =  soup.find('picture', 'item-comic-image')
+
+imgSource = img.find("img")
+imgURL = imgSource['src']
+print(imgURL)
