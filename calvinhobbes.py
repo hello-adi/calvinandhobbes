27a5247325c59ed63ff096ed945c43d54587325a
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-04-04 16:29:42 -0700

    Cleaned up code

--- calvinhobbes.py
+++ calvinhobbes.py
@@ -1,31 +1,25 @@
-#Calvin and hobbes comic strip downloader
+# Calvin and hobbes comic strip downloader
 
-#imports
+# imports
 import requests
 from bs4 import BeautifulSoup
 import wget
 
+# page url
+
 page = requests.get('https://www.gocomics.com/random/calvinandhobbes')
-#page url
-#print(page.url)
 
-#BeautifulSoup object
+# BeautifulSoup object with lxml parser
 soup = BeautifulSoup(page.content, 'lxml')
-#print(soup.prettify())
 
-#extracting image url class_=img-fluid lazyloaded
+# extracting image url class_=img-fluid lazyloaded
+img = soup.find('picture', 'item-comic-image')
 
-# <picture class="item-comic-image"><img alt="Calvin and Hobbes Comic Strip for April 09, 2012 " class="lazyload img-fluid" data-srcset="https://assets.amuniversal.com/d5712420c125012fdd6d001dd8b71c47 900w" sizes="
-#                        (min-width: 992px) 900px,
-#                        (min-width: 768px) 600px,
-#                        (min-width: 576px) 300px,
-#                        900px" src="https://assets.amuniversal.com/d5712420c125012fdd6d001dd8b71c47" srcset="https://assets.gocomics.com/assets/transparent-3eb10792d1f0c7e07e7248273540f1952d9a5a2996f4b5df70ab026cd9f05517.png" width="100%"/></picture>
-img =  soup.find('picture', 'item-comic-image')
-
+# getting image source by finding element by id
 imgSource = img.find("img")
+
+# get image url by using src tag
 imgURL = imgSource['src']
 
-#image url
-#print(imgURL)
-
+# downloading image to pc using wget.
 wget.download(imgURL)
